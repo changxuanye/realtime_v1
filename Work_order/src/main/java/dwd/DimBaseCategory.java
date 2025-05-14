e73b814b5596df4b7b2e722c10208dceafc940be
常选业 <14643133+frequently-selected-industries@user.noreply.gitee.com>
2025-05-14 22:48:48 +0800

    5.14

---
+++ Work_order/src/main/java/dwd/DimBaseCategory.java
@@ -0,0 +1,17 @@
+package dwd;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class DimBaseCategory implements Serializable {
+    private String id;
+    private String b3name;
+    private String b2name;
+    private String b1name;
+}
